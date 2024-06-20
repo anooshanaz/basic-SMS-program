64300f10246d211d4a0a792baebb095c4ac62e31
Anoosha Naz <163389690+anooshanaz@users.noreply.github.com>
2024-06-20 06:38:20 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.yellowBright("Student application form"));
+let answer = await inquirer.prompt([
+    {
+        name: "student",
+        type: "string",
+        message: chalk.redBright("Student Name")
+    },
+    {
+        name: "fathername",
+        type: "string",
+        message: chalk.redBright("Father Name")
+    },
+    {
+        name: "Class",
+        type: "number|string",
+        message: chalk.redBright("Qualification")
+    },
+    {
+        name: "Gender",
+        type: "list",
+        message: chalk.redBright("Gender"),
+        choices: ["Male", "Female"]
+    },
+    {
+        name: "Phone no",
+        type: "number",
+        message: chalk.redBright("Phone no")
+    },
+    {
+        name: "Email",
+        type: "string|number",
+        message: chalk.redBright("Email Address")
+    },
+    {
+        name: "Password",
+        type: "number|string",
+        message: chalk.redBright("Password")
+    },
+    {
+        name: "Cources",
+        type: "list",
+        message: chalk.redBright("Select you cource"),
+        choices: ["CIT", "Graphic designing", "Digital marketing", "Freelancing", "Web development"]
+    },
+    {
+        name: "Amount",
+        type: "list",
+        message: chalk.redBright("Select Amount"),
+        choices: [1000, 2000, 3000]
+    },
+]);
+console.log(chalk.yellowBright("Received application Successfully"));
+let answer2 = await inquirer.prompt([{
+        name: "table",
+        type: "number",
+        message: ("write a table of 2")
+    }]);
---
+++ index.ts
@@ -0,0 +1,56 @@
+
+import inquirer from "inquirer";
+import chalk from "chalk"
+import { table } from "console";
+console.log(chalk.yellowBright("Student application form"))
+let answer = await inquirer.prompt([
+    {
+    name: "student",
+    type: "string",
+    message: chalk.redBright("Student Name")
+    },
+    {
+    name: "fathername",
+    type: "string",
+    message: chalk.redBright("Father Name")
+    },
+    {
+    name: "Class",
+    type: "number|string",
+    message: chalk.redBright("Qualification")
+    },
+    {
+    name: "Gender",
+    type: "list",
+    message: chalk.redBright("Gender"),
+    choices: ["Male","Female"]
+    },
+    {
+    name: "Phone no",
+    type: "number",
+    message: chalk.redBright ("Phone no")
+    },
+    {
+    name: "Email",
+    type: "string|number",
+    message: chalk.redBright("Email Address")
+    },
+    {
+    name: "Password",
+    type: "number|string",
+    message: chalk.redBright("Password")
+    },
+    {
+    name: "Cources",
+    type: "list",
+    message: chalk.redBright("Select you cource"),
+    choices: ["CIT","Graphic designing","Digital marketing","Freelancing","Web development"]
+    },
+    {
+    name: "Amount",
+    type: "list",
+    message: chalk.redBright("Select Amount"),
+    choices: [1000,2000,3000]
+    },
+]);
+console.log(chalk.yellowBright("Received application Successfully"))
